62e251e99b78f0491cbff2f4d0151f7a8e28b070
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2023-09-21 07:24:29 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "Kanalga Qoshilish",
                 url = client.invitelink)
         ]
     ]
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'Qoshildim',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
