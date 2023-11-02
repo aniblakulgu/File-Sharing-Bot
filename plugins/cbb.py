eacf82179a46b2cb3271794cc53fbcc58f4830ce
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2023-11-02 07:57:52 +0500

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Kanal : @AnidonUz\n○ Asosiy Kanal : @Anime_Uzbekcha</b>",
+            text = f"<b>○ Admin : @KING_MODDER_UZ\n○ Kanal : @UZGOANIME</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
